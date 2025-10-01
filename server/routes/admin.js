f02859b65003a332cfa4db837b09953e1e9b987a
Ahmed Maajid <ahamedmaajid87@gmail.comgit commit -m Initial>
2025-10-01 14:30:58 +0530

    Route for Create admin Automatically 2

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -30,7 +30,26 @@ const upload = multer({ dest: "uploads/" });
 
 const adminRoutes = express.Router();
 adminRoutes.get("/get-commerce-data", getCommerceData)
+// For Creating an initial admin user
+adminRoutes.get("/create-admin", async (req, res) => {
+    try {
+        const password = "admin123"; // strong one in real life
 
+        const adminUser = new User({
+            firstName: "Zainul",
+            lastName: "Hamthy",
+            email: "hamthyzainul4@gmail.com",
+            password: password,
+            isAdmin: true,
+        });
+
+        await adminUser.save();
+        res.json({ message: "Admin created", admin: adminUser });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: "Error creating admin" });
+    }
+});
 adminRoutes.use(protect, requireAdmin);
 adminRoutes.get("/isAdmin", isAdmin)
 adminRoutes.post("/addCategory", upload.single("image"), addCategory);
@@ -76,26 +95,7 @@ adminRoutes.delete("/variation/:id", deleteVariation)
 
 
 
-// For Creating an initial admin user
-adminRoutes.get("/create-admin", async (req, res) => {
-    try {
-        const password = "admin123"; // strong one in real life
 
-        const adminUser = new User({
-            firstName: "Zainul",
-            lastName: "Hamthy",
-            email: "hamthyzainul4@gmail.com",
-            password: password,
-            isAdmin: true,
-        });
-
-        await adminUser.save();
-        res.json({ message: "Admin created", admin: adminUser });
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({ message: "Error creating admin" });
-    }
-});
 
 
 adminRoutes.post("/add-shipping-details", addShippingDetails)
